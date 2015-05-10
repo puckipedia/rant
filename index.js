228d2568f2f90d915538e4ee9154611c835cc2db
Puck Meerburg <puck@puckipedia.nl>
2015-05-10 22:06:27 +0200

    Add rant range

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var rant_range;
+var current_rant;
+
+function load() {
+	rant_range = document.getElementById("rant_range");
+	current_rant = document.getElementById("current");
+	update_rant();
+}
+
+function update_rant() {
+	current_rant.innerHTML = rant_range.value;
+}
+
 function make_it_rant() {
 	var rant_button = document.getElementById("make_it_rant");
 	var rant = document.getElementById("rant");
@@ -5,7 +18,7 @@ function make_it_rant() {
 	rant.value = "";
 	for (var word_i in to_make_ranty) {
 		var word = to_make_ranty[word_i];
-		if (Math.random() < 0.2)
+		if (Math.random() <= (1.0 / rant_range.value))
 			word += " FUCKING";
 		rant.value += word + " ";
 	}
